72def3d43d285ab0edd5dcf2b61d0644f27b6d5e
Samuel Sylvester <samsylvester09@gmail.com>
2023-12-14 09:58:04 +0100

    Create main.h

---
+++ main.h
@@ -0,0 +1,21 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h> 
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#define MAX_SIZE 1024
+#define MAX_TOKENS 32
+
+void tokenize(char *input, char *tokens[]);
+
+#endif
