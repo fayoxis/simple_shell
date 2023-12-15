a88ddccdf6d8c2fd36d1c617557822bfd72bb41a
Samuel Sylvester <samsylvester09@gmail.com>
2023-12-15 23:59:55 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -33,12 +33,21 @@ char *name;
 void (*func)(char **args);
 } CommandFunction;
 
-extern char **environ;
+/*extern char **environ;
 extern char *input_line;
 extern char **command;
 extern char *shell_alias;
 extern char **cmd_args;
-extern int status;
+extern int status;*/
+
+typedef struct {
+    char **command;
+    char *input_line;
+    char *shell_alias;
+    int exit_status;
+} Shellinfo;
+
+Shellinfo shell_value = {NULL, NULL, NULL, 0};
 
 void print_prompt(const char *string, int fd);
 void remove_newline(char *str);
