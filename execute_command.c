40c4c583d9f55f4bfd5c961a3d4af84b6d0c3d15
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2023-12-15 23:07:25 +0100

    Update execute_command.c

--- execute_command.c
+++ execute_command.c
@@ -11,21 +11,21 @@ int status;
  */
 void initialize_command(char **current_args, int type_cmd)
 {
-    pid_t pid;
+pid_t pid;
 
-    if (type_cmd == COMMAND_EXTERNAL || type_cmd == COMMAND_PATH)
-    {
-        pid = fork();
-        if (pid == 0)
-            exe_command(current_args, type_cmd);
-        else
-        {
-            waitpid(pid, &status, 0);
-            status >>= 8;
-        }
-    }
-    else
-        exe_command(current_args, type_cmd);
+if (type_cmd == COMMAND_EXTERNAL || type_cmd == COMMAND_PATH)
+{
+pid = fork();
+if (pid == 0)
+exe_command(current_args, type_cmd);
+else
+{
+waitpid(pid, &status, 0);
+status >>= 8;
+}
+}
+else
+exe_command(current_args, type_cmd);
 }
 
 /**
@@ -37,29 +37,28 @@ void initialize_command(char **current_args, int type_cmd)
  */
 void exe_command(char **tokenized_args, int cmd_type)
 {
-    void (*cmd_func)(char **);
-
-    if (cmd_type == COMMAND_EXTERNAL)
-    {
-        execute_external_command(tokenized_args);
-    }
-    else if (cmd_type == COMMAND_PATH)
-    {
-        execute_path_command(tokenized_args);
-    }
-    else if (cmd_type == COMMAND_INTERNAL)
-    {
-        cmd_func = command_func(tokenized_args[0]);
-        cmd_func(tokenized_args);
-    }
-    else if (cmd_type == COMMAND_INVALID)
-    {
-        print_prompt(shell_alias, STDERR_FILENO);
-		print_prompt(": 1: ", STDERR_FILENO);
-		print_prompt(tokenized_args[0], STDERR_FILENO);
-		print_prompt(": No such command\n", STDERR_FILENO);
-		status = 127;
-    }
+void (*cmd_func)(char **);
+if (cmd_type == COMMAND_EXTERNAL)
+{
+execute_external_command(tokenized_args);
+}
+else if (cmd_type == COMMAND_PATH)
+{
+execute_path_command(tokenized_args);
+}
+else if (cmd_type == COMMAND_INTERNAL)
+{
+cmd_func = command_func(tokenized_args[0]);
+cmd_func(tokenized_args);
+}
+else if (cmd_type == COMMAND_INVALID)
+{
+print_prompt(shell_alias, STDERR_FILENO);
+print_prompt(": 1: ", STDERR_FILENO);
+print_prompt(tokenized_args[0], STDERR_FILENO);
+print_prompt(": No such command\n", STDERR_FILENO);
+status = 127;
+}
 }
 
 /**
@@ -70,11 +69,11 @@ void exe_command(char **tokenized_args, int cmd_type)
  */
 void execute_external_command(char **args)
 {
-    if (execve(args[0], args, NULL) == -1)
-    {
-        perror("error");
-        exit(2);
-    }
+if (execve(args[0], args, NULL) == -1)
+{
+perror("error");
+exit(2);
+}
 }
 
 /**
@@ -85,9 +84,9 @@ void execute_external_command(char **args)
  */
 void execute_path_command(char **args)
 {
-    if (execve(command_path(args[0]), args, NULL) == -1)
-    {
-        perror("error");
-        exit(2);
-    }
+if (execve(command_path(args[0]), args, NULL) == -1)
+{
+perror("error");
+exit(2);
+}
 }
