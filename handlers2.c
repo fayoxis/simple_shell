f932e00d1a92c21d8163fb01da49a783b80c7706
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2023-12-15 22:16:38 +0100

    Update handlers2.c

--- handlers2.c
+++ handlers2.c
@@ -8,34 +8,33 @@
  * Return: no of bytes in the initial segment of `str` which are part of `accept`
  */
 int _strspn(char *str, const char *accept) {
-    int i = 0;
-    int match = 0;
-
-    while (str[i] != '\0') {
-        if (_strchr(accept, str[i]) == NULL)
-            break;
-        match++;
-        i++;
-    }
-    return match;
+int i = 0;
+int match = 0;
+while (str[i] != '\0') {
+if (_strchr(accept, str[i]) == NULL)
+break;
+match++;
+i++;
+}
+return (match);
 }
 
 /**
- * _strcspn - computes segment of `str` which consists of characters not in `reject`
+ * _strcspn - computes segment of `str`
+ * which consists of characters not in `reject`
  * @str: string to be searched
  * @reject: string containing characters to reject
  *
  * Return: index at which a char in `str` exists in `reject`
  */
 int _strcspn(char *str, const char *reject) {
-    int len = 0, i;
-
-    for (i = 0; str[i] != '\0'; i++) {
-        if (_strchr(reject, str[i]) != NULL)
-            break;
-        len++;
-    }
-    return len;
+int len = 0, i;
+for (i = 0; str[i] != '\0'; i++) {
+if (_strchr(reject, str[i]) != NULL)
+break;
+len++;
+}
+return (len);
 }
 
 /**
@@ -46,14 +45,13 @@ int _strcspn(char *str, const char *reject) {
  * Return: pointer to the first occurrence of `c` in `s`
  */
 const char *_strchr(const char *s, char c) {
-    int i = 0;
-
-    for (; s[i] != c && s[i] != '\0'; i++)
-        ;
-    if (s[i] == c)
-        return (s + i);
-    else
-        return NULL;
+int i = 0;
+for (; s[i] != c && s[i] != '\0'; i++)
+;
+if (s[i] == c)
+return ((s + i));
+else
+return (NULL);
 }
 
 
@@ -66,11 +64,10 @@ const char *_strchr(const char *s, char c) {
  */
 void _strcpy(char *src, char *dest)
 {
-    int i = 0;
-
-    for (; src[i] != '\0'; i++)
-        dest[i] = src[i];
-    dest[i] = '\0';
+int i = 0;
+for (; src[i] != '\0'; i++)
+dest[i] = src[i];
+dest[i] = '\0';
 }
 
 /**
@@ -82,15 +79,14 @@ void _strcpy(char *src, char *dest)
  */
 int _strcmp(char *str1, char *str2)
 {
-    int i = 0;
-
-    while (str1[i] != '\0')
-    {
-        if (str1[i] != str2[i])
-            break;
-        i++;
-    }
-    return (str1[i] - str2[i]);
+int i = 0;
+while (str1[i] != '\0')
+{
+if (str1[i] != str2[i])
+break;
+i++;
+}
+return ((str1[i] - str2[i]));
 }
 
 /**
@@ -102,13 +98,12 @@ int _strcmp(char *str1, char *str2)
  */
 char *_strcat(char *dest, char *src)
 {
-    char *result = NULL;
-    int len_dest = _strlen(dest);
-    int len_src = _strlen(src);
-
-    result = malloc(sizeof(*result) * (len_dest + len_src + 1));
-    _strcpy(dest, result);
-    _strcpy(src, result + len_dest);
-    result[len_dest + len_src] = '\0';
-    return result;
+char *result = NULL;
+int len_dest = _strlen(dest);
+int len_src = _strlen(src);
+result = malloc(sizeof(*result) * (len_dest + len_src + 1));
+_strcpy(dest, result);
+_strcpy(src, result + len_dest);
+result[len_dest + len_src] = '\0';
+return (result);
 }
