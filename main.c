852f8b83bcd7735ba536b41802617974f9edffc7
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2023-12-15 22:29:36 +0100

    Update main.c

--- main.c
+++ main.c
@@ -16,43 +16,41 @@ int exit_status = 0;
  */
 
 int main(int argc __attribute__((unused)), char **argv) {
-    char **current_args = NULL;
-    int i, command_type = 0;
-    size_t buffer_size = 0;
+char **current_args = NULL;
+int i, command_type = 0;
+size_t buffer_size = 0;
+signal(SIGINT, handle_interrupt);
+shell_alias = argv[0];
 
-    signal(SIGINT, handle_interrupt);
-    shell_alias = argv[0];
+while (1) {
+non_interact();
+print_prompt("$ ", STDOUT_FILENO);
+if (getline(&input_line, &buffer_size, stdin) == -1) {
+free(input_line);
+exit(exit_status);
+}
 
-    while (1) {
-        non_interact();
-        print_prompt("$ ", STDOUT_FILENO);
+remove_newline(input_line);
+remove_comment(input_line);
+command = tokenize(input_line, ";");
+for (i = 0; command[i] != NULL; i++) {
+current_args = tokenize(command[i], " ");
+if (current_args[0] == NULL)
+{
+free(current_args);
+break;
+}
+command_type = classify_command(current_args[0]);
 
-        if (getline(&input_line, &buffer_size, stdin) == -1) {
-            free(input_line);
-            exit(exit_status);
-        }
+/* initialize command */
+initialize_command(current_args, command_type);
+free(current_args);
+}
+free(command);
+}
 
-        remove_newline(input_line);
-        remove_comment(input_line);
-        command = tokenize(input_line, ";");
-
-        for (i = 0; command[i] != NULL; i++) {
-            current_args = tokenize(command[i], " ");
-            if (current_args[0] == NULL) {
-                free(current_args);
-                break;
-            }
-            command_type = classify_command(current_args[0]);
-
-            /* initialize command */
-            initialize_command(current_args, command_type);
-            free(current_args);
-        }
-        free(command);
-    }
-
-    free(input_line);
-    return (exit_status);
+free(input_line);
+return ((exit_status));
 }
 
 
@@ -62,11 +60,9 @@ int main(int argc __attribute__((unused)), char **argv) {
  * @fd: file descriptor to print to
  */
 void print_prompt(const char *prompt, int fd) {
-    size_t len = 0;
-
-    while (prompt[len] != '\0') {
-        len++;
-    }
-
-    write(fd, prompt, len);
+size_t len = 0;
+while (prompt[len] != '\0') {
+len++;
+}
+write(fd, prompt, len);
 }
