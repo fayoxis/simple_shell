a2af68cf61f5061e7021c89b95be2532a5112e82
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2023-12-15 23:17:56 +0100

    Update no_interact.c

--- no_interact.c
+++ no_interact.c
@@ -10,14 +10,18 @@ size_t buffer_size = 0;
 int exit_status = 0;
 int i;
 
-if (!isatty(STDIN_FILENO)) {
-while (getline(&input_line, &buffer_size, stdin) != -1) {
+if (!isatty(STDIN_FILENO))
+{
+while (getline(&input_line, &buffer_size, stdin) != -1)
+{
 remove_newline(input_line);
 remove_comment(input_line);
 cmd_args = tokenize(input_line, ";");
-for (i = 0; cmd_args[i] != NULL; i++) {
+for (i = 0; cmd_args[i] != NULL; i++)
+{
 current_args = tokenize(cmd_args[i], " ");
-if (current_args[0] == NULL) {
+if (current_args[0] == NULL)
+{
 free(current_args);
 break;
 }
