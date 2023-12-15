1d73f37baf54505cd81c1e3c363e3eb7a359b98a
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2023-12-15 22:47:13 +0100

    Update shell.c

--- shell.c
+++ shell.c
@@ -10,30 +10,25 @@
  * Return: The next available token
  */
 char *_strtok(char *str, const char *delim, char **save_ptr) {
-    char *token_start;
-    char *token_end;
+char *token_start;
+char *token_end;
+if (str == NULL)
+str = *save_ptr;
+token_start = str + _strspn(str, delim);
 
-    if (str == NULL)
-        str = *save_ptr;
+if (*token_start == '\0') {
+*save_ptr = token_start;
+return (NULL);
+}
 
-    token_start = str + _strspn(str, delim);
-
-    if (*token_start == '\0') {
-        *save_ptr = token_start;
-        return (NULL);
-    }
-
-    token_end = token_start + _strcspn(token_start, delim);
-
-    if (*token_end == '\0') {
-        *save_ptr = token_end;
-        return token_start;
-    }
-
-    *token_end = '\0';
-    *save_ptr = token_end + 1;
-
-    return token_start;
+token_end = token_start + _strcspn(token_start, delim);
+if (*token_end == '\0') {
+*save_ptr = token_end;
+return (token_start);
+}
+*token_end = '\0';
+*save_ptr = token_end + 1;
+return (token_start);
 }
 
 /**
@@ -45,29 +40,36 @@ char *_strtok(char *str, const char *delim, char **save_ptr) {
  * Return: pointer to the address of the new memory block
  */
 void *_realloc(void *ptr, size_t old_size, size_t new_size) {
-    void *new_block;
-    size_t i;
-
-    if (ptr == NULL) {
-        new_block = malloc(new_size);
-        return new_block;
-    } else if (new_size == old_size) {
-        return ptr;
-    } else if (new_size == 0 && ptr != NULL) {
-        free(ptr);
-        return NULL;
-    } else {
-        new_block = malloc(new_size);
-        if (new_block != NULL) {
-            for (i = 0; i < old_size && i < new_size; i++) {
-                *((char *)new_block + i) = *((char *)ptr + i);
-            }
-            free(ptr);
-            return new_block;
-        } else {
-            return NULL;
-        }
-    }
+void *new_block;
+size_t i;
+if (ptr == NULL) {
+new_block = malloc(new_size);
+return new_block;
+} 
+else if (new_size == old_size)
+{
+return (ptr);
+} 
+else if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+} 
+else
+{
+new_block = malloc(new_size);
+if (new_block != NULL) {
+for (i = 0; i < old_size && i < new_size; i++) {
+*((char *)new_block + i) = *((char *)ptr + i);
+}
+free(ptr);
+return (new_block);
+} 
+else
+{
+return (NULL);
+}
+}
 }
 
 
@@ -81,22 +83,18 @@ void *_realloc(void *ptr, size_t old_size, size_t new_size) {
  */
 
 char **tokenize(char *input_string, const char *delim) {
-    int num_tokens = 0;
-    char **tokens = NULL;
-    char *token;
-    static char *save_ptr;  
-
-    token = _strtok(input_string, delim, &save_ptr);
-
-    while (token != NULL) {
-        tokens = _realloc(tokens, sizeof(*tokens) * num_tokens, sizeof(*tokens) * (num_tokens + 1));
-        tokens[num_tokens] = token;
-        token = _strtok(NULL, delim, &save_ptr);  
-        num_tokens++;
-    }
-
-    tokens = _realloc(tokens, sizeof(*tokens) * num_tokens, sizeof(*tokens) * (num_tokens + 1));
-    tokens[num_tokens] = NULL;
-
-    return tokens;
+int num_tokens = 0;
+char **tokens = NULL;
+char *token;
+static char *save_ptr;  
+token = _strtok(input_string, delim, &save_ptr);
+while (token != NULL) {
+tokens = _realloc(tokens, sizeof(*tokens) * num_tokens, sizeof(*tokens) * (num_tokens + 1));
+tokens[num_tokens] = token;
+token = _strtok(NULL, delim, &save_ptr);  
+num_tokens++;
+}
+tokens = _realloc(tokens, sizeof(*tokens) * num_tokens, sizeof(*tokens) * (num_tokens + 1));
+tokens[num_tokens] = NULL;
+return (tokens);
 }
