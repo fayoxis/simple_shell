2e9147561d2e96da9347e37a744447f55dfe7eac
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2023-12-16 03:22:18 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -41,6 +41,10 @@ extern char **command;
 extern char *shell_alias;
 extern char **cmd_args;
 extern int status;
+char **command = NULL;
+char *input_line = NULL;
+char *shell_alias;
+int exit_status = 0;
 
 
 
